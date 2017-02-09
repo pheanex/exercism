97e33bbcbdaaeebbf973c70c74f3ac614c5b3acb
Konstantin Manna <Konstantin@Manna.uno>
2017-02-09 19:40:37 +0100

    added triangle solution

---
+++ python/triangle/triangle.py
@@ -0,0 +1,20 @@
+class Triangle:
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+        if self.x <= 0 or self.y <= 0 or self.z <= 0 or\
+           self.x + self.y <= self.z or\
+           self.x + self.z <= self.y or\
+           self.y + self.z <= self.x:
+            raise TriangleError
+
+    def kind(self):
+        if self.x == self.y == self.z:
+            return 'equilateral'
+        elif len({self.x, self.y, self.z}) == 2:
+            return 'isosceles'
+        else:
+            return 'scalene'
+
+
+class TriangleError(Exception):
+    pass
