2630313e139564a6e5fddbb8f831b6bebff40af7
Konstantin Manna <Konstantin@Manna.uno>
2017-02-12 20:29:02 +0100

    improve binary search solution

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,14 +1,12 @@
 def binary_search(list, element):
-    if not list:
-        raise ValueError
     left, right = 0, len(list) - 1
     mid = (left + right) // 2
-    while not list[mid] == element:
+    while left <= right:
         if list[mid] < element:
             left = mid + 1
-        else:
+        elif list[mid] > element:
             right = mid - 1
-        if left > right:
-            raise ValueError
+        else:
+            return mid
         mid = (left + right) // 2
-    return mid
+    raise ValueError
