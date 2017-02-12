69a6e9aab974dc0795f4f51e30503cf3b490664f
Konstantin Manna <Konstantin@Manna.uno>
2017-02-12 21:52:45 +0100

    added list-exercise solution

--- python/linked-list/linked_list.py
+++ python/linked-list/linked_list.py
@@ -1,6 +1,3 @@
-# Skeleton file for the Python "linked-list" exercise.
-
-
 class Node(object):
     def __init__(self, value, next=None, prev=None):
         self.value = value
@@ -10,4 +7,36 @@ class Node(object):
 
 class LinkedList(object):
     def __init__(self):
-        pass  # Complete the Deque class ...
+        self.first = self.last = None
+
+    def push(self, element):
+        if not self.last:
+            self.last = self.first = Node(element)
+        else:
+            self.last = Node(element, prev=self.last)
+            self.last.prev.next = self.last
+
+    def pop(self):
+        if not self.last:
+            raise ValueError
+        val = self.last.value
+        self.last = self.last.prev
+        if self.last:
+            self.last.next = None
+        return val
+
+    def shift(self):
+        if not self.first:
+            raise ValueError
+        val = self.first.value
+        self.first = self.first.next
+        if self.first:
+            self.first.prev = None
+        return val
+
+    def unshift(self, element):
+        if not self.first:
+            self.last = self.first = Node(element)
+        else:
+            self.first = Node(element, next=self.first)
+            self.first.next.prev = self.first
