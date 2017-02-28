8126bc2c74ab9a2dafad7f41039ea4c1f1fb6775
Konstantin Manna <Konstantin@Manna.uno>
2017-02-28 07:59:04 +0100

    Add solution for perfect numbers

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,1 +1,5 @@
-exercism fetch python secret-handshake
+def is_perfect(number):
+    divisors = [n for n in range(1, int(number/2+1)) if not number % n]
+    if sum(divisors) == number:
+        return True
+    return False
