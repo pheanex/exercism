91b25b8d8d1ddb9b02c64bb29d9416d95d1bda43
Konstantin Manna <Konstantin@Manna.uno>
2017-02-28 07:44:26 +0100

    Updated Queen Attack solution

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,12 +1,15 @@
-def board(white, black):
-    if any(x for x in white + black if x > 7 or x < 0) or white == black:
+def valid_position(white, black):
+    if any(x for x in white + black if not 0 <= x < 8) or white == black:
         raise ValueError
-    return [''.join('_' if not ((x, y) in [white, black]) else 'W' if (x, y) == white else 'B' for y in range(0, 8)) for x in range(0, 8)]
+
+
+def board(white, black):
+    valid_position(white, black)
+    return [''.join('_' if not (x, y) in [white, black] else 'W' if (x, y) == white else 'B' for y in range(0, 8)) for x in range(0, 8)]
 
 
 def can_attack(white, black):
-    if any(x for x in white + black if x > 7 or x < 0) or white == black:
-        raise ValueError
+    valid_position(white, black)
     if white[0] == black[0] or white[1] == black[1] or abs(white[0]-black[0]) == abs(white[1]-black[1]):
         return True
     return False
