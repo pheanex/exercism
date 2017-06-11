388f8b77ebd7ae343168e38a46ea61a473adb0e2
Konstantin Manna <Konstantin@Manna.uno>
2017-06-11 21:40:15 +0200

    reversed protein dicitionary

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,31 +1,30 @@
+protein_to_codons = {
+    'Methionine': ['AUG'],
+    'Phenylalanine': ['UUU', 'UUC'],
+    'Leucine': ['UUA', 'UUG'],
+    'Serine': ['UCU', 'UCC', 'UCA', 'UCG'],
+    'Tyrosine': ['UAU', 'UAC'],
+    'Cysteine': ['UGU', 'UGC'],
+    'Tryptophan': ['UGG'],
+    'STOP': ['UAA', 'UAG', 'UGA']
+}
+codon_to_protein = dict((v, k) for k in protein_to_codons for v in protein_to_codons[k])
+
+
 def of_codon(codon):
-    CodonToPolypeptide = {'AUG': 'Methionine',
-                          'UUU': 'Phenylalanine',
-                          'UUC': 'Phenylalanine',
-                          'UUA': 'Leucine', 
-                          'UUG': 'Leucine',
-                          'UCU': 'Serine',
-                          'UCC': 'Serine',
-                          'UCA': 'Serine',
-                          'UCG': 'Serine',
-                          'UAU': 'Tyrosine',
-                          'UAC': 'Tyrosine',
-                          'UGU': 'Cysteine',
-                          'UGC': 'Cysteine',
-                          'UGG': 'Tryptophan',
-                          'UAA': 'STOP',
-                          'UAG': 'STOP',
-                          'UGA': 'STOP'}
-    if codon not in CodonToPolypeptide:
+    if codon not in codon_to_protein:
         raise ValueError
-    return CodonToPolypeptide[codon]
+    return codon_to_protein[codon]
 
 
-def of_rna(string):
-    chunks = [string[i:i + 3] for i in range(0, len(string), 3)]
-    polipeptides = []
-    for chunk in chunks:
-        if of_codon(chunk) == 'STOP':
+def of_rna(sequence):
+    proteins = []
+    for codon in map(of_codon, chunkstring(sequence, 3)):
+        if codon == 'STOP':
             break
-        polipeptides.append(of_codon(chunk))
-    return polipeptides
+        proteins.append(codon)
+    return proteins
+
+
+def chunkstring(string, size):
+    return [string[i:i + size] for i in range(0, len(string), size)]
