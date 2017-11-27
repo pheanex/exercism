074efe75fb9eb082036052cf04877a46145ce880
Konstantin Manna <Konstantin@Manna.uno>
2017-11-27 13:56:59 +0100

    update all-your-base solution

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,20 +1,19 @@
 def rebase(from_base, digits, to_base):
     if from_base < 2:
         raise ValueError
-    if any(digit for digit in digits if digit < 0 or digit >= from_base):
+    if not all(0 <= digit < from_base for digit in digits):
         raise ValueError
     if to_base < 2:
         raise ValueError
 
-    number_length = len(digits)
     sum = 0
     for index, digit in enumerate(digits, start=1):
-        power = number_length - index
+        power = len(digits) - index
         sum += digit * pow(from_base, power)
 
     digits = []
     while sum:
-        quotient, remainder = divmod(sum, to_base)
-        digits.append(remainder)
-        sum = quotient
+        digits.append(sum % to_base)
+        sum //= to_base
+
     return digits[::-1]
