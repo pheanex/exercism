9b374d2dc19fe7835153b78066f650e2ceb93299
Konstantin Manna <konstantin.manna@rewe-digital.com>
2017-12-21 12:19:18 +0100

    refactor transpose: remove "line" from variable-names

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -3,7 +3,7 @@ from itertools import zip_longest
 
 def transpose(input_lines):
     lines = input_lines.splitlines()
-    encoded_lines = [line.replace(' ', '_') for line in lines]
-    encoded_transposed_lines = [''.join(line) for line in zip_longest(*encoded_lines, fillvalue=' ')]
-    decoded_tranposed_lines = [line.rstrip().replace('_', ' ') for line in encoded_transposed_lines]
-    return '\n'.join(decoded_tranposed_lines)
+    encoded = [line.replace(' ', '_') for line in lines]
+    encoded_transposed = [''.join(line) for line in zip_longest(*encoded, fillvalue=' ')]
+    decoded_transposed = [line.rstrip().replace('_', ' ') for line in encoded_transposed]
+    return '\n'.join(decoded_transposed)
