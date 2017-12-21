29755f4bcf8394351b853edf4b31b533c213c959
PHeanEX <github@pheanex.de>
2017-12-21 19:16:56 +0100

    add exercise "tree-building" and implement solution

---
+++ python/tree-building/tree_building.py
@@ -0,0 +1,34 @@
+class Record:
+    def __init__(self, record_id, parent_id):
+        self.node_id = record_id
+        self.parent_id = parent_id
+
+
+class Node:
+    def __init__(self, node_id):
+        self.node_id = node_id
+        self.children = []
+
+
+def BuildTree(records):
+    nodes = {}
+    if set(record.node_id for record in records) != set(range(len(records))):
+        raise ValueError('Index not continuous')
+    for record in records:
+        nodes[record.node_id] = Node(record.node_id)
+    for record in sorted(records, key=lambda r: r.node_id):
+        parent_id = record.parent_id
+        child_id = record.node_id
+        if child_id == 0 and parent_id != 0:
+            raise ValueError('Parent of root is not itself')
+        if child_id < parent_id:
+            raise ValueError('Parent ID > Child ID')
+        if 0 != child_id == parent_id:
+            raise ValueError('Cycle')
+        if child_id == parent_id == 0:
+            continue
+        parent = nodes[parent_id]
+        child = nodes[child_id]
+        parent.children.append(child)
+
+    return nodes[0] if records else None
