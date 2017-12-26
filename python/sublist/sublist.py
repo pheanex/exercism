fee9ad22b1724d9057a787a0d9ebe8293ec43f5f
PHeanEX <github@pheanex.de>
2017-12-26 23:37:39 +0100

    fix sublist solution

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -2,7 +2,7 @@ SUBLIST, SUPERLIST, EQUAL, UNEQUAL = 0, 1, 2, 3
 
 
 def check_lists(a, b):
-    a, b = [''.join(['{}'.format(c) for c in l]) for l in [a, b]]
+    a, b = [','.join(['{}'.format(c) for c in l]) for l in [a, b]]
     if a == b:
         return EQUAL
     elif a in b:
