12128434cc484e5d28c10ce77db5353d792f5b1a
PHeanEX <github@pheanex.de>
2017-12-27 13:22:19 +0100

    update perfect-numbers tests and implementation

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,9 @@
-def is_perfect(number):
-    return sum(n for n in range(1, number // 2 + 1) if number % n == 0) == number
+def classify(number):
+    if number < 1:
+        raise ValueError('Number not in range')
+    sum_of_factors = sum(n for n in range(1, number // 2 + 1) if number % n == 0)
+    if sum_of_factors == number:
+        return 'perfect'
+    if sum_of_factors > number:
+        return 'abundant'
+    return 'deficient'
