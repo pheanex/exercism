c3624e581bd4bbb29aee9fd6a3ac7203c629d37b
PHeanEX <github@pheanex.de>
2017-12-27 19:00:03 +0100

    update testdata for binary exercise

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -1,5 +1,5 @@
 def parse_binary(string):
     if set(string) - set('01'):
-        raise ValueError
+        raise ValueError('string not in binary format')
 
     return sum([2**i for i, j in enumerate(string[::-1]) if j == '1'])
