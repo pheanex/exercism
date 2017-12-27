e5daa252c8f2c54d456ef86e3b9cba62c8b7879d
PHeanEX <github@pheanex.de>
2017-12-27 18:03:38 +0100

    update all-your-base exercise testdata and solution

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,10 +1,10 @@
 def rebase(from_base, digits, to_base):
     if from_base < 2:
-        raise ValueError
+        raise ValueError('from_base too small')
     if not all(0 <= digit < from_base for digit in digits):
-        raise ValueError
+        raise ValueError('digits do not match from_base')
     if to_base < 2:
-        raise ValueError
+        raise ValueError('to_base too small')
 
     sum = 0
     for index, digit in enumerate(digits, start=1):
