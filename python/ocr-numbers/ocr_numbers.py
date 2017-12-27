3591569b292ed0e219ab09d9686acb87eab5156b
PHeanEX <github@pheanex.de>
2017-12-27 13:16:36 +0100

    update ocr_numbers tests and implementation

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -10,20 +10,19 @@ ocr_numbers = {'0': (' _ ', '| |', '|_|', '   '),
                '9': (' _ ', '|_|', ' _|', '   ')}
 
 
-def number(line):
-    return ''.join(digit(d) for d in zip(*[[r[i:i + 3] for i in range(0, len(r), 3)] for r in line]))
+def convert(lines):
+    blocks = [lines[i:i+4] for i in range(0, len(lines), 4)]
+    return ','.join(convert_block(block) for block in blocks)
 
 
-def digit(m):
+def convert_block(block):
+    return ''.join(convert_digit(d) for d in zip(*[[r[i:i + 3] for i in range(0, len(r), 3)] for r in block]))
+
+
+def convert_digit(m):
     if len(m) != 4 or [i for i in m if len(i) != 3]:
-        raise ValueError
+        raise ValueError('Error')
     for key, value in ocr_numbers.items():
         if m == value:
             return key
     return '?'
-
-
-def grid(digits):
-    if any(i for i in digits if not i.isdigit()):
-        raise ValueError
-    return [''.join(i) for i in list(zip(*[list(ocr_numbers[n]) for n in digits]))]
