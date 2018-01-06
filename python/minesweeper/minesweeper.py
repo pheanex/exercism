58348d318580c8acaa0a08227441c02eaa9a243b
Konstantin Manna <Konstantin@Manna.uno>
2018-01-06 21:24:52 +0100

    update tests an implementation for exercise 'minesweeper'

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,22 +1,19 @@
 def board(matrix):
-    def column(lists, i):
-        return [l[i] for l in lists]
-
     def increment_block(x, y):
-        for i in range(x - 1, x + 2):
-            for j in range(y - 1, y + 2):
-                counter_matrix[i][j] += 1
+        for i in [i for i in range(x - 1, x + 2) if i >= 0]:
+            for j in [j for j in range(y - 1, y + 2) if j >= 0]:
+                if i < len(counter_matrix) and j < len(counter_matrix[i]):
+                    counter_matrix[i][j] += 1
 
-    if not matrix[0][0] == matrix[0][-1] == matrix[-1][0] == matrix[-1][-1] == '+' or \
-            set(matrix[0][1:-1] + matrix[-1][1:-1]) != set('-') or \
-            set(column(matrix[1:-1], 0) + column(matrix[1:-1], -1)) != set('|') or \
-            any(len(line) != len(matrix[0]) or set(line) - (set("+-|* ")) for line in matrix):
-        raise ValueError
+    if len(set(len(row) for row in matrix)) > 1:
+        raise ValueError('Rows have different length')
 
     counter_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
     for row_index, row in enumerate(matrix):
         for column_index, char in enumerate(row):
             if char == '*':
                 increment_block(row_index, column_index)
+            if char not in [' ', '*']:
+                raise ValueError('Invalid char')
 
     return [''.join([str(c) if d == ' ' and c != 0 else d for c, d in zip(a, b)]) for a, b in zip(counter_matrix, matrix)]
