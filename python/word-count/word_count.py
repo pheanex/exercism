fd4443e543539f8a15621048a277ad34b72dfb35
PHeanEX <github@pheanex.de>
2018-01-09 15:46:43 +0100

    update tests and implementation for exercise 'word-count'

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -4,8 +4,8 @@ from collections import Counter
 
 def word_count(sentence):
     word_counter = Counter()
-    split_sentence = re.split('\W|\s|_', sentence.lower())
+    split_sentence = re.split(r"[^a-zA-Z0-9']", sentence.lower().replace('.', '').replace(':', ''))
     for word in filter(None, split_sentence):
-        word_counter[word] += 1
+        word_counter[word.strip("'")] += 1
 
     return dict(word_counter)
