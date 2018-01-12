278ef819c8d7915c576d74c9f42c79ede254de98
PHeanEX <github@pheanex.de>
2018-01-12 19:46:06 +0100

    update tests and implementation for exercise luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,6 @@
 class Luhn:
     def __init__(self, number):
-        self.checknumber = int(str(number)[:-1])
+        self.checknumber = str(number)[:-1].replace(' ', '')
         self.checkdigit = int(str(number)[-1])
 
     def addends(self):
@@ -9,18 +9,14 @@ class Luhn:
     def checksum(self):
         return sum(self.addends())
 
-    @property
     def is_valid(self):
-        return False if self.checksum() % 10 else True
-
-    @staticmethod
-    def create(number):
-        return int(str(number) +
-                   str((10 - sum(Luhn.double_number(number)) % 10) % 10))
+        if len(self.checknumber) < 1 or not all(char.isdigit() for char in self.checknumber) or self.checksum() % 10 != 0:
+            return False
+        return True
 
     @staticmethod
     def double_number(number):
-        number = list(map(int, str(number)))
+        number = list(map(int, number))
         for counter, value in enumerate(number[::-1]):
             index = len(number) - counter - 1
             if counter % 2 == 0:
